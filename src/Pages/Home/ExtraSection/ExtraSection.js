7dfceab4b6ba0de2d6588a8e9a96a4a4d8600a72
Tasmim Rahman <tasmimrahman2000@gmail.com>
2022-11-25 01:39:21 +0600

    navber added

---
+++ src/Pages/Home/ExtraSection/ExtraSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ExtraSection = () => {
+    return (
+        <div>
+               <div>
+               <div className="flex flex-col w-full lg:flex-row mt-12 mb-12 justify-around bg-orange-600 pb-12 pt-12">
+  <div><h2 className='font-bold text-3xl  text-white'>Sell fast with less work</h2></div>
+  <div><input type="text" placeholder="SellCell Trade-In" className="input  w-96  text-white input-light" /></div>
+</div>
+               </div>
+               <div>
+               <div className="flex flex-col w-full lg:flex-row mt-12 mb-12 justify-around bg-violet-600 pb-12 pt-12">
+  <div><h2 className='font-bold text-3xl  text-white'>Buy Phone in less price</h2></div>
+  <div><input type="text" placeholder="SellCell Trade-In" className="input  w-96  text-white input-light" /></div>
+</div>
+               </div>
+        
+        </div>
+    );
+};
+
+export default ExtraSection;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExtraSection from './ExtraSection/ExtraSection';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>this is home page</h2>
+            <ExtraSection></ExtraSection>
+        </div>
+    );
+};
+
+export default Home;
