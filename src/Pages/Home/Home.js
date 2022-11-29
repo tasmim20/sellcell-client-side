4fcf180eebc5a3fe9e050543f7b63b3e5f5c3a24
Tasmim Rahman <tasmimrahman2000@gmail.com>
2022-11-29 17:43:56 +0600

    res added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <h2 className='text-3xl  my-16 text-center font-bold '>Welcome to the safest marketplace for selling and buying. </h2>
-           <div className='grid lg:grid-cols-3  sm:grid-cols-1 gap-10 ' >
+           <div className='grid lg:grid-cols-3  md:grid-cols-2 sm:grid-cols-1 gap-10 ' >
             {
                 products.map(product => <Product key={product._id} product={product}></Product>)
             }
--- src/Pages/MyOrders/MyOrders.js
+++ src/Pages/MyOrders/MyOrders.js
@@ -20,7 +20,7 @@ const MyOrders = () => {
     return (
         <div>
            <h2 className='text-center text-2xl my-10 font-bold text-green-700'>My Orders</h2>
-     <div className='grid lg:grid-cols-3 sm:grid-cols-1 gap-10 my-10'>
+     <div className='grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-10 my-10'>
      {
             bookings.map( booking => <MyOrder key={booking._id} booking ={booking}></MyOrder>)
            }
