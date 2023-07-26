5d6b3f67af2d17325851cf15311039e4a9e6872a
Tasmim Rahman <tasmimrahman2000@gmail.com>
2023-07-26 18:02:12 +0600

    dsfs

--- src/Register/SignUp/SignUp.js
+++ src/Register/SignUp/SignUp.js
@@ -40,7 +40,7 @@ const SignUp = () => {
 
     const saveUser = (name, email) =>{
         const user ={name, email};
-        fetch('http://localhost:5000/users', {
+        fetch('https://sellcell-server.vercel.app/users', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
@@ -55,7 +55,7 @@ const SignUp = () => {
     }
 
     const getUserToken = email =>{
-        fetch(`http://localhost:5000/jwt?email=${email}`)
+        fetch(`https://sellcell-server.vercel.app/jwt?email=${email}`)
         .then(res => res.json())
         .then(data =>{
             if(data.accessToken){
